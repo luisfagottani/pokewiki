b24a6d82207aeee75f67990d171d977fa91b2aac
Luis <asp.r2.agottani@gmail.com>
2021-03-16 23:35:06 -0300

    Resizing header/menu component

--- src/modules/Menu/components/Item.jsx
+++ src/modules/Menu/components/Item.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 import style from './Item.module.scss';
 
 const Item = ({ label = 'Default', route = '/' }) => (
-  <NavLink to={route} activeClassName={style.item}>
+  <NavLink to={route} className={style.item} activeClassName={style['item--active']}>
     <span className={style.item__label}>{label}</span>
   </NavLink>
 );

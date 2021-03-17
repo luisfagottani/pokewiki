48d1bfa99a1fb680533660ca9caadfab57121d61
Luis <asp.r2.agottani@gmail.com>
2021-03-17 00:54:53 -0300

    Desktop variation pokeblocks

--- src/modules/PokeBlocks/components/PokeBlock.jsx
+++ src/modules/PokeBlocks/components/PokeBlock.jsx
@@ -2,9 +2,9 @@ import { ReactComponent as PokeballIcon } from 'assets/images/pokeball-icon.svg'
 
 import style from './PokeBlock.module.scss';
 
-const PokeBlock = ({ onClick, label = 'Pokemon', alt = 'Alt image', imgSrc = '' }) => (
+const PokeBlock = ({ label = 'Pokemon', alt = 'Alt image', imgSrc = '' }) => (
   <li className={style.pokeblock}>
-    <div tabIndex="0" onClick={onClick} role="button" className={style.pokeblock__content}>
+    <a href={'123'} className={style.pokeblock__content}>
       <figure className={style.pokeblock__figure}>
         {imgSrc ? (
           <img src={imgSrc} alt={alt} className={style.pokeblock__image} />
@@ -13,7 +13,7 @@ const PokeBlock = ({ onClick, label = 'Pokemon', alt = 'Alt image', imgSrc = '' 
         )}
         <figcaption className={style.pokeblock__caption}>{label}</figcaption>
       </figure>
-    </div>
+    </a>
   </li>
 );
 
